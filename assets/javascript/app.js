376a41e8dfa345a744ccc80445df178b58c50221
Daniel Ward <dward1502@gmail.com>
2017-12-03 01:10:22 -0800

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,50 +10,56 @@
     let time = 15;
     let timer;
     let currentQuestion;
-    let unanswered;
+    let unanswered = 0;
     let userSelect;
     let answered = false;
 
 //objects
-var triviaQuestions = [
+var QandA = [
 
     {
         question: "Who was the best multiplayer character in Goldeneye 007?" ,
         answerArr: [" Bond", " Jaws", " Oddjob", " Xenia"],
-        answers: 2
+        answers: 2,
+        image: "assets/images/question1.gif" 
     },{
         question: "What was the most annoying weapon to get hit by in Mario Kart 64?",
         answerArr: [" Thunderbolt", " Spiny (blue) shell", " Red Shell", " Banana peel" ],
-        answers: 3
+        answers: 3,
+        image: "assets/images/question2.gif"
     },{
         question:" Hardest temple in The Legend of Zelda: Ocarina of Time?",
         answerArr: [" Fire", " Water", " Shadow", " Forest"],
-        answers :2
+        answers :2,
+        image: "assets/images/question3.gif"
     },{
         question: " Best selling N64 Game?",
         answerArr: [" Super Mario 64", " Goldeneye 007", " Super Smash Bros", " Donkey Kong 64"],
-        answers: 0
+        answers: 0,
+        image: "assets/images/question4.gif"
     },{
         question: " Which Star Wars N64 game was the best?",
         answerArr: [" Rogue Squadron", " Shadows of the Empire", " Episode I Racer", " X Wing Alliance"],
-        answers: 1
+        answers: 1,
+        image: "assets/images/question5.gif"
     }, {
         question:  " Best sports game on N64?",
         answerArr: [" 1080 Snowboarding", " Wave Racer 64", " NFL Blitz 2000", " Tony Hawk Pro Skater 2"],
-        answers: 3
+        answers: 3,
+        image: "assets/images/question6.gif"
     },{
         question: "Best multiplayer game on N64?",
         answerArr: ["Perfect Dark", "Goldeneye 007", "Super Smash Bros", "Mario Kart 64", ],
-        answers: 1
+        answers: 1,
+        image: " assets/images/question7.gif"
             
 }];
 var messages ={
-        correct:"Questions you answered correctly: ",
-        incorrect:"Questions you answered incorrectly",
+        correct:"You answered correctly! ",
+        incorrect:"Incorrect answer!",
         timeEnd:"Out of time, good luck next time!",
         finished:"Trivia is all done lets see how well you did!"
 }
-//var imgArr = [ question1, question2 ,question3 ,question4 ,question5 ,question6 ,question7  ];
 
 //Functions 
 //=============================================
@@ -73,12 +79,12 @@ function newQuestion(){
     $("#correctedAnswer").empty();
     $("#image").empty();
     $("#answerList").empty();
-    answered = true;
-    $("#question").html(triviaQuestions[currentQuestion].question);
+    
+    $("#question").html(QandA[currentQuestion].question);
     timeRem();
     for( var i = 0; i < 4; i++){
         var choices = $("<div>");
-        choices.text(triviaQuestions[currentQuestion].answerArr[i]);
+        choices.text(QandA[currentQuestion].answerArr[i]);
         choices.attr({'data-index':i});
         choices.addClass('thisChoice');
         $('#answerList').append(choices);
@@ -87,34 +93,39 @@ function newQuestion(){
         userSelect = $(this).data('index');
         clearInterval(timer);
         answerPage();
+        answered = true;
     });
 
 }
 function answerPage(){
     $("#question").empty();
     $('.thisChoice').empty();
-    var rightAnswer = triviaQuestions[currentQuestion].answerArr[triviaQuestions[currentQuestion].answers];
-    var rightAnswerIndex = triviaQuestions[currentQuestion].answers;
-    if((userSelect === rightAnswerIndex) && (answered = true) ){
+    var rightAnswer = QandA[currentQuestion].answerArr[QandA[currentQuestion].answers];
+    var rightAnswerIndex = QandA[currentQuestion].answers;
+    //$("#image").html(QandA[currentQuestion].image);
+    //.prepend('<img id="theImg" src="theImg.png" />')
+    $("#image").html( '<img src =' + QandA[currentQuestion].image +'>');
+    if(answered === false){
+        unanswered++;
+        
+        console.log("unanswered" + unanswered);
+        $("#message").html(messages.timeEnd);
+        $("#correctedAnswer").html("The correct answer was" + rightAnswer);
+
+    }else if((userSelect === rightAnswerIndex) && (answered = true) ){
         correct++;
         console.log("correct" + correct);
-        //$("#message").html(messages.correct);
+        $("#message").html(messages.correct);
         
     }else if((userSelect != rightAnswerIndex) && (answered = true)){
         wrong++;
         console.log("wrong" + wrong);
         $("#message").html(messages.incorrect);
         $("#correctedAnswer").html("The correct answer was" + rightAnswer);
-        //answered = true;
-    } else{
-        unanswered++;
-        answered = false;
-        console.log("unanswered" + unanswered);
-        $("#message").html(messages.timeEnd);
-        $("#correctedAnswer").html("The correct answer was" + rightAnswer);
+         //answered = true;
     }
 
-    if(currentQuestion == (triviaQuestions.length -1)){
+    if(currentQuestion == (QandA.length -1)){
        setTimeout(endGame,5000)
     }else{
         currentQuestion++;
@@ -129,6 +140,7 @@ function endGame(){
     $("#correct").empty();
     $("#startOverBtn").addClass('reset');
     $("#startOverBtn").show();
+    $("#image").empty();
     $("#endMessage").html(messages.finished);
     $("#correct").html("Number Correct: " + correct);
     $("#wrong").html("Number Wrong: " + wrong);
